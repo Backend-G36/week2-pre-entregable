b54f5c80847bb1f452c2700494f7d812f4ab25ca
Gabriel Martinez <martinezgabrielalejandro4@gmail.com>
2024-06-01 00:05:01 -0300

    advances

--- src/controllers/song.controllers.js
+++ src/controllers/song.controllers.js
@@ -6,8 +6,55 @@ const getAll = catchError(async (req, res) => {
   return res.json(result)
 });
 
+const create = catchError(async (req, res) => {
+  const result = await Song.create(req.body)
+  return res.status(201).json(result)
+})
+
+const getOne = catchError(async (req, res) => {
+  const { id } = req.params
+  const result = await Song.findByPk(id)
+  if (!result) return res.sendStatus(404)
+  return res.json(result)
+})
+
+const destroy = catchError(async (req, res) => {
+  const { id } = req.params
+  const result = await Song.destroy({ where: { id } })
+  if (!result) return res.sendStatus(404)
+  return res.sendStatus(204)
+})
+
+const update = catchError(async (req, res) => {
+  const { id } = req.params
+
+  const result = await Song.update(
+    req.body,
+    { where: { id }, returning: true }
+  )
+  if (result[0] === 0) return res.sendStatus(404)
+
+  return res.json(result[1][0])
+})
+
+// const update = catchError(async (req, res) => {
+//   const { id } = req.params
+//   const preUpdate = await Song.findByPk(id)
+
+//   const result = await preUpdate.update(req.body)
+
+//   if (!result) return res.sendStatus(404)
+
+//   return res.json(result)
+
+// })
+
 module.exports = {
-  getAll
+  getAll,
+  create,
+  getOne,
+  destroy,
+  update
 }
 
 /**
--- src/routes/song.router.js
+++ src/routes/song.router.js
@@ -1,9 +1,18 @@
-const { getAll } = require('../controllers/song.controllers');
+const { getAll, create, getOne, destroy, update } = require('../controllers/song.controllers');
 const express = require('express');
 
 const songRouter = express.Router();
 
+//rutas estaticas
 songRouter.route("/")
   .get(getAll)
+  .post(create)
+
+
+//rutas dinamicas
+songRouter.route('/:id')
+  .get(getOne)
+  .delete(destroy)
+  .put(update)
 
 module.exports = songRouter;
